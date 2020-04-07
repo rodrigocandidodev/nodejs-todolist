e47c9bcd4aeda9826f04456bdfbe5d01da83c974
Rodrigo Cândido <rodrigo_candido.dev@outlook.com>
2020-04-07 08:13:05 -0300

    Cors added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express   = require('express');
 const routes    = require('./routes');
+const cors      = require('cors');
 
 const app   = express();
 require('./database/index');
 const port  = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api',routes);
 app.listen(port);
